840bcd28ce3c1548d6b89782cfb55df2b7e66064
X project <107910326+Xprj3ct@users.noreply.github.com>
2023-03-23 06:35:17 +0100

    Update index.js

--- email-form-api/index.js
+++ email-form-api/index.js
@@ -8,34 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
 // enable CORS
-app.use(cors({
-  origin: 'https://construction-wine.vercel.app'
-}));
+app.use(cors());
 
 app.post("/send-email", (req, res) => {
   const { fullname, email, phone, message } = req.body;
@@ -51,7 +25,7 @@ app.post("/send-email", (req, res) => {
   const mailOptions = {
     from: "Contact Us Service <y@gmail.com>",
     to: "info@luckylumacs.com",
-    subject: "Enquiries from your website!",
+    subject: "A New message from your website!",
     html: `
       <p>Full Name: ${fullname}</p>
       <p>Email: ${email}</p>
